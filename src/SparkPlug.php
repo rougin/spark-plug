b617944803c5c19202f08a5d29a377348bb0217a
Rougin Gutib <rougingutib@gmail.com>
2016-11-20 22:45:25 +0800

    Improve code quality (#1)

--- src/SparkPlug.php
+++ src/SparkPlug.php
@@ -2,11 +2,6 @@
 
 namespace Rougin\SparkPlug;
 
-use CI_Controller;
-use FilesystemIterator;
-use RecursiveIteratorIterator;
-use RecursiveDirectoryIterator;
-
 /**
  * Spark Plug
  *
@@ -23,14 +18,14 @@ class SparkPlug
     protected $globals = [];
 
     /**
-     * @var array
-     */
-    protected $server = [];
-
-    /**
      * @var string
      */
     protected $path = '';
+
+    /**
+     * @var array
+     */
+    protected $server = [];
 
     /**
      * @param array  $globals
@@ -40,8 +35,8 @@ class SparkPlug
     public function __construct(array &$globals, array $server, $path = '')
     {
         $this->globals =& $globals;
-        $this->server = $server;
-        $this->path = $path;
+        $this->path    = (empty($path)) ? getcwd() : $path;
+        $this->server  = $server;
     }
 
     /**
@@ -51,29 +46,20 @@ class SparkPlug
      */
     public function getCodeIgniter()
     {
-        $this->setPaths();
-        $this->setEnvironment();
-        $this->loadConstants();
-        $this->loadClasses();
-        $this->setCharSet();
-
-        // Sets global configurations
-        $this->globals['CFG'] =& load_class('Config', 'core');
-        $this->globals['UNI'] =& load_class('Utf8', 'core');
-        $this->globals['SEC'] =& load_class('Security', 'core');
-
-        // Loads the CodeIgniter's core classes
-        load_class('Loader', 'core');
-        load_class('Router', 'core');
-        load_class('Input', 'core');
-        load_class('Lang', 'core');
+        $this->setPathConstants();
+        $this->prepareEnvironment();
+        $this->defineConstants();
+        $this->loadCommonClasses();
+        $this->setCharacterSets();
+        $this->setConfigurations();
+        $this->loadCoreClasses();
 
         // Loads the get_instance.php for loading libraries
         require 'get_instance.php';
 
-        $ci = CI_Controller::get_instance();
+        $ci = \CI_Controller::get_instance();
 
-        return (empty($ci)) ? new CI_Controller : $ci;
+        return (empty($ci)) ? new \CI_Controller : $ci;
     }
 
     /**
@@ -81,13 +67,11 @@ class SparkPlug
      *
      * @return void
      */
-    protected function loadClasses()
+    protected function loadCommonClasses()
     {
         require BASEPATH . 'core/Common.php';
 
-        if (! class_exists('CI_Controller')) {
-            require BASEPATH . 'core/Controller.php';
-        }
+        class_exists('CI_Controller') || require BASEPATH . 'core/Controller.php';
     }
 
     /**
@@ -95,20 +79,49 @@ class SparkPlug
      *
      * @return void
      */
-    protected function loadConstants()
+    protected function defineConstants()
     {
-        if (defined('FILE_READ_MODE')) {
-            return;
+        $constants   = APPPATH . 'config/constants.php';
+        $environment = APPPATH . 'config/' . ENVIRONMENT . '/constants.php';
+        $filename    = file_exists($environment) ? $environment : $constants;
+
+        defined('FILE_READ_MODE') || require $filename;
+    }
+
+    /**
+     * Loads the CodeIgniter's core classes.
+     *
+     * @return void
+     */
+    protected function loadCoreClasses()
+    {
+        load_class('Loader', 'core');
+        load_class('Router', 'core');
+        load_class('Input', 'core');
+        load_class('Lang', 'core');
+    }
+
+    /**
+     * Sets the base path.
+     *
+     * @return void
+     */
+    protected function setBasePath()
+    {
+        $directory = new \RecursiveDirectoryIterator(getcwd());
+        $iterator  = new \RecursiveIteratorIterator($directory);
+
+        foreach ($iterator as $item) {
+            $core = 'core' . DIRECTORY_SEPARATOR . 'CodeIgniter.php';
+
+            if (strpos($item->getPathname(), $core) !== false) {
+                $path = str_replace($core, '', $item->getPathname());
+
+                define('BASEPATH', $path);
+
+                break;
+            }
         }
-
-        $envConstants = APPPATH . 'config/' . ENVIRONMENT . '/constants.php';
-        $constants = APPPATH . 'config/constants.php';
-
-        if (file_exists($envConstants)) {
-            $constants = $envConstants;
-        }
-
-        require $constants;
     }
 
     /**
@@ -116,15 +129,13 @@ class SparkPlug
      *
      * @return void
      */
-    protected function setCharSet()
+    protected function setCharacterSets()
     {
         $charset = strtoupper(config_item('charset'));
 
         ini_set('default_charset', $charset);
 
-        if (! defined('MB_ENABLED')) {
-            define('MB_ENABLED', extension_loaded('mbstring'));
-        }
+        defined('MB_ENABLED') || define('MB_ENABLED', extension_loaded('mbstring'));
 
         // mbstring.internal_encoding is deprecated starting with PHP 5.6
         // and it's usage triggers E_DEPRECATED messages.
@@ -132,16 +143,7 @@ class SparkPlug
             ini_set('mbstring.internal_encoding', $charset);
         }
 
-        // This is required for mb_convert_encoding() to strip invalid
-        // characters. That's utilized by CI_Utf8, but it's also done for
-        // consistency with iconv.
-        mb_substitute_character('none');
-
-        // There's an ICONV_IMPL constant, but the PHP manual says that using
-        // iconv's predefined constants is "strongly discouraged".
-        if (! defined('ICONV_ENABLED')) {
-            define('ICONV_ENABLED', extension_loaded('iconv'));
-        }
+        $this->setIconv();
     }
 
     /**
@@ -149,7 +151,7 @@ class SparkPlug
      *
      * @return void
      */
-    protected function setEnvironment()
+    protected function prepareEnvironment()
     {
         $environment = 'development';
 
@@ -157,9 +159,36 @@ class SparkPlug
             $environment = $this->server['CI_ENV'];
         }
 
-        if (! defined('ENVIRONMENT')) {
-            define('ENVIRONMENT', $environment);
-        }
+        defined('ENVIRONMENT') || define('ENVIRONMENT', $environment);
+    }
+
+    /**
+     * Sets global configurations.
+     *
+     * @return void
+     */
+    protected function setConfigurations()
+    {
+        $this->globals['CFG'] =& load_class('Config', 'core');
+        $this->globals['UNI'] =& load_class('Utf8', 'core');
+        $this->globals['SEC'] =& load_class('Security', 'core');
+    }
+
+    /**
+     * Sets up ICONV.
+     *
+     * @return void
+     */
+    protected function setIconv()
+    {
+        // This is required for mb_convert_encoding() to strip invalid
+        // characters. That's utilized by CI_Utf8, but it's also done for
+        // consistency with iconv.
+        mb_substitute_character('none');
+
+        // There's an ICONV_IMPL constant, but the PHP manual says that using
+        // iconv's predefined constants is "strongly discouraged".
+        defined('ICONV_ENABLED') || define('ICONV_ENABLED', extension_loaded('iconv'));
     }
 
     /**
@@ -167,45 +196,18 @@ class SparkPlug
      *
      * @return void
      */
-    protected function setPaths()
+    protected function setPathConstants()
     {
-        $applicationPath = realpath('application');
-        $vendorPath = realpath('vendor');
+        $paths = [
+            'APPPATH'  => $this->path . '/application/',
+            'VENDOR'   => $this->path . '/vendor/',
+            'VIEWPATH' => $this->path . '/application/views/',
+        ];
 
-        if ($this->path) {
-            $vendorPath = $this->path . '/vendor';
-            $applicationPath = $this->path . '/application';
+        foreach ($paths as $key => $value) {
+            defined($key) || define($key, $value);
         }
 
-        if (! defined('APPPATH')) {
-            define('APPPATH', $applicationPath . '/');
-        }
-
-        if (! defined('VENDOR')) {
-            define('VENDOR', $vendorPath . '/');
-        }
-
-        if (! defined('VIEWPATH')) {
-            define('VIEWPATH', APPPATH . '/views/');
-        }
-
-        if (defined('BASEPATH')) {
-            return;
-        }
-
-        $directory = new RecursiveDirectoryIterator(getcwd());
-        $iterator = new RecursiveIteratorIterator($directory);
-
-        foreach ($iterator as $file) {
-            $core = 'core' . DIRECTORY_SEPARATOR . 'CodeIgniter.php';
-
-            if (strpos($file->getPathname(), $core) !== false) {
-                $path = str_replace($core, '', $file->getPathname());
-
-                define('BASEPATH', $path);
-
-                break;
-            }
-        }
+        defined('BASEPATH') || $this->setBasePath();
     }
 }
